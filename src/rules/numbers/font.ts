014cf763cf19b61ffd7d47d34441728cbd62bcab
zhuxianyong <zhuxianyong@baidu.com>
2023-08-01 12:36:27 +0800

    增加 font-weight

--- src/rules/numbers/font.ts
+++ src/rules/numbers/font.ts
@@ -1,10 +1,11 @@
 import { NumStyle } from '../../interfaces/styles';
-import { PX, VAL_REG_STR } from '../../shared/const';
+import { PX, VAL_REG_STR, INT_REG_STR } from '../../shared/const';
 import { getNumStyles } from '../../shared/utils';
 
 const MAP = new Map<RegExp, string>([
     [new RegExp(`^fs-${VAL_REG_STR}$`), 'font-size: {n}{u};'],
-    [new RegExp(`^lh-${VAL_REG_STR}$`), 'line-height: {n}{u};']
+    [new RegExp(`^lh-${VAL_REG_STR}$`), 'line-height: {n}{u};'],
+    [new RegExp(`^fw-${INT_REG_STR}$`), 'font-weight: {n};']
 ]);
 
 export default function font(classNames: Set<string>): NumStyle[] {
